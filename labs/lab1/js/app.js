e9e149d7d6b429035121e7f0cb051825a68770e5
ZachThomas-iu <zachtho374@gmail.com>
2020-09-03 03:24:23 -0400

    Adding lab 1

---
+++ labs/lab1/js/app.js
@@ -0,0 +1,19 @@
+function setup(){
+    createCanvas(800, 600)
+
+    background("gray");
+
+//Haku's colors
+    var green = "#5fe4a0";
+    var yellow = "#fcf6a7";
+    var white = "#dbebea";
+    var gray = "#c8cdd1";
+    var pink = "#e99d9c";
+
+//Haku body
+    fill(white);
+    noStroke();
+    quad(100, 300, 150, 330, 200, 350, 280, 350);
+    quad(100, 300, 50, 350, 170, 400, 280, 350);
+    quad(170, 400, )
+}
